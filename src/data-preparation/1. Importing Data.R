e844b37f5ed4d404fb773a2f55cb835db3e203c6
Stefan-Valentijn <stef.valentijn@gmail.com>
2025-09-15 17:03:33 +0200

    New importing data script

---
+++ src/data-preparation/1. Importing Data.R
@@ -0,0 +1,7 @@
+library(googledrive)
+
+#Import the IMDb 'basics' file
+raw_basics <- read.csv("https://drive.google.com/uc?export=download&id=1kibqUiY63nTgU40dYh3WYGYMnU3HpGpv")
+
+#Import the IMDb 'ratings' file
+raw_ratings <- read.csv("https://drive.google.com/uc?export=download&id=1tvvAQKNL6OTTiHc9xwzxkydWupzKMXJs")
