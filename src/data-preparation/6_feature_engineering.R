8543d0b02b73c7ec650a4326de16b85a8e410f62
Stefan-Valentijn <stef.valentijn@gmail.com>
2025-09-26 15:09:09 +0200

    Update the names of the analysis folder + updated paths

--- src/data-preparation/6_feature_engineering.R
+++ src/data-preparation/6_feature_engineering.R
@@ -6,20 +6,20 @@ library(tidyverse)
 #Read in data
 movies_filtered <- read_csv("../../gen/temp/movies_filtered.csv")
 
-movies <- movies_filtered
-
 #Dummy for release year 2010 since that is part of the focus of the research
-movies$before_2010_dummy <- ifelse(movies$startYear <= 2010, 1, 0)
-movies$before_2010_dummy <- factor(movies$before_2010_dummy, levels = c(0, 1), labels = c("Release since 2010", "Release before 2010"))
-table(movies$before_2010_dummy)
+movies_filtered$before_2010_dummy <- ifelse(movies_filtered$startYear <= 2010, 1, 0)
+movies_filtered$before_2010_dummy <- factor(movies_filtered$before_2010_dummy, levels = c(0, 1), labels = c("Release since 2010", "Release before 2010"))
+table(movies_filtered$before_2010_dummy)
 
 #Dummy for animation since that is part of the focus of the research
-movies$animation_dummy <- ifelse(grepl("Animation", movies$genres), 1, 0)
-movies$animation_dummy <- factor(movies$animation_dummy, levels = c(0, 1), labels = c("Non-Animation", "Animation"))
-table(movies$animation_dummy)
+movies_filtered$animation_dummy <- ifelse(grepl("Animation", movies_filtered$genres), 1, 0)
+movies_filtered$animation_dummy <- factor(movies_filtered$animation_dummy, levels = c(0, 1), labels = c("Non-Animation", "Animation"))
+table(movies_filtered$animation_dummy)
 
 #Create folder where the documents will be stored locally
 dir.create("../../gen/output", recursive = TRUE)
 
+movies_prepped <- movies_filtered
+
 #Save the definitive dataset as a file
-write.csv(movies, file = "../../gen/output/movies.csv", row.names = FALSE)
+write.csv(movies_prepped, file = "../../gen/temp/movies_prepped.csv", row.names = FALSE)
